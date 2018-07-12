80d024aad631e2408b5431ee3044841b889d9c6c
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2018-07-11 21:28:15 -0700

    config output

--- ZipCode/main.cpp
+++ ZipCode/main.cpp
@@ -2,10 +2,20 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 #include "ZipCode.h"
 
 using namespace std;
+
+ofstream csis;
+
+void printMyInfo() {                             //print my student ID and compiler details
+    csis << "Name: Aileen Cacayorin" << endl;
+    csis << "Palomar ID: 011519388" << endl;
+    csis << "Title: Constructors Lab (Bar Codes)" << endl;
+    csis << "Compiler: XCode 9.2" << endl;
+}
 
 int main()
 {
@@ -24,6 +34,19 @@ int main()
     cout << zip4.getZipCode() << setw(35) << zip4.getBarCode() << endl;
     cout << zip5.getZipCode() << setw(35) << zip5.getBarCode() << endl;
     cout << zip6.getZipCode() << setw(35) << zip6.getBarCode() << endl;
+    
+    // Code Output
+    csis.open("csis.txt");
+    printMyInfo();
+    csis << "Digits" << "       " << "Bar Code" << endl;
+    csis << zip1.getZipCode() << setw(35) << zip1.getBarCode() << endl;
+    csis << zip2.getZipCode() << setw(35) << zip2.getBarCode() << endl;
+    csis << zip3.getZipCode() << setw(35) << zip3.getBarCode() << endl;
+    csis << endl;
+    csis << zip4.getZipCode() << setw(35) << zip4.getBarCode() << endl;
+    csis << zip5.getZipCode() << setw(35) << zip5.getBarCode() << endl;
+    csis << zip6.getZipCode() << setw(35) << zip6.getBarCode() << endl;
+    csis.close();
 
     return 0;
 }
